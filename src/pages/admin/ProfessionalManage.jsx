191911486f5970f863789154dae581480ffdad83
imlinkseo <imlinkseo@gmail.com>
2024-08-27 17:44:25 +0900

    [ADD] card style

--- src/pages/admin/ProfessionalManage.jsx
+++ src/pages/admin/ProfessionalManage.jsx
@@ -5,9 +5,11 @@ export default function ProfessionalManage() {
   return (
     <main>
       <h2 css={pageTitle}>구성원 관리</h2>
-      <div css={card}>
-        <p css={cardTitle}>전체</p>
-        <p css={cardContent}>
+      <div css={card} className="selected">
+        <p css={cardTitle} className="cardTitle">
+          전체
+        </p>
+        <p css={cardContent} className="cardContent">
           <span className="num">16</span>
           <span>명</span>
         </p>
@@ -22,6 +24,7 @@ const pageTitle = css`
   font-weight: 700;
   line-height: 44px;
   color: var(--mono-black-title);
+  margin-bottom: 20px;
 `;
 
 const card = css`
@@ -34,6 +37,15 @@ const card = css`
   padding: 16px;
   box-sizing: border-box;
   border-radius: 10px;
+  &.selected {
+    background-color: var(--point-color-1);
+    .cardTitle {
+      color: var(--mono-white);
+    }
+    .cardContent * {
+      color: var(--mono-white);
+    }
+  }
 `;
 
 const cardTitle = css`
